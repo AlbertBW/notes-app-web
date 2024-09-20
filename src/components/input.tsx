573ba717ad752c0ccb55e21ba40d5f5267ed18ec
Albert Wales <41163950+AlbertBW@users.noreply.github.com>
2024-09-20 16:17:31 +0100

    Codemirror with markdown/languages

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,3 +1,10 @@
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { default as ReactMarkdown } from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { useCallback } from "react";
+
 export default function Input({
   note,
   writeNote,
@@ -10,14 +17,33 @@ export default function Input({
   } | null;
   writeNote: (noteId: number, content: string) => void;
 }) {
+  const onChange = useCallback(
+    (val: string) => {
+      if (note) writeNote(note.id, val);
+    },
+    [note, writeNote]
+  );
+
   return (
-    <div className="w-full bg-black z-50">
+    <div className=" w-full bg-black z-50 overflow-y-scroll transition-colors">
       {note && (
-        <input
-          className="w-full bg-black"
-          value={note.content}
-          onChange={(e) => writeNote(note.id, e.target.value)}
-        />
+        <div className="w-full h-full">
+          <div className="flex w-full h-full">
+            <ReactCodeMirror
+              value={note.content}
+              theme={"dark"}
+              minHeight="100%"
+              onChange={onChange}
+              extensions={[
+                markdown({ base: markdownLanguage, codeLanguages: languages }),
+              ]}
+              className="w-1/2 h-full codemirror overflow-y-scroll"
+            />
+            <ReactMarkdown remarkPlugins={[remarkGfm]} className="w-1/2">
+              {note.content}
+            </ReactMarkdown>
+          </div>
+        </div>
       )}
 
       {!note && (
